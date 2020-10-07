687cbfbcadbde54032a086604fff813a9fd898a9
Vero <>
2020-10-07 23:14:50 +0200

    Messed around with CMake
    I have a working mini library and executable that builds itself in CMake and creates a solution for Visual Studio.

---
+++ Test_CMake/apps/Test_CMake.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Test_CMake/Print.hpp>
+
+
+int main()
+{
+    Print("Hello, World!");
+
+    system("pause");
+    return 0;
+}
---
+++ Test_CMake/include/Test_CMake/Print.hpp
@@ -0,0 +1,18 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <iostream>
+
+template<typename T>
+inline void Print(const T& input)
+{
+    std::cout << input << std::endl;
+}
+
+
+
+
+
+
+
+#endif
